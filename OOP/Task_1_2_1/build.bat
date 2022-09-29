51268d9f381f247a2e4806e93e2a808cbb012955
jokerhasnopersonality <105290479+jokerhasnopersonality@users.noreply.github.com>
2022-09-29 20:55:23 +0700

    Delete OOP directory

--- OOP/Task_1_2_1/build.bat
+++
@@ -1,5 +0,0 @@
-mkdir Task_1_2_1/bin
-javac -sourcepath Task_1_2_1/src -d Task_1_2_1/bin Task_1_2_1/src/main/java/io/github/jokerhasnopersonality/HeapSort.java
-
-mkdir Task_1_2_1/doc
-javadoc -d Task_1_2_1/doc -charset utf-8 -sourcepath Task_1_2_1/src/main/java -subpackages io.github.jokerhasnopersonality
