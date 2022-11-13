7f9b0e13ec11c1bc488abbdedcd88ba044009e0d
Q Taynaya <3773050@mail.ru>
2022-11-13 20:49:11 +0700

    Added tests, changed search methods.

--- Task_1_3_1/src/main/java/io/github/jokerhasnopersonality/StringSearch.java
+++ Task_1_3_1/src/main/java/io/github/jokerhasnopersonality/StringSearch.java
@@ -1,55 +1,135 @@
 package io.github.jokerhasnopersonality;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.util.ArrayList;
-
+import static java.lang.Math.max;
 import static java.lang.Math.min;
 
-public class StringSearch {
-    int z_function(String string, BufferedReader reader) throws IOException {
-        int len = string.length();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
-        ArrayList<Integer> ZZ = new ArrayList<>(len);
+
+/**
+ * String Search class for searching substrings in text files.
+ * Search methods are case-sensitive.
+ */
+public class StringSearch {
+    /**
+     * Searches the given substring in the given file.
+     * For each line builds z function and adds substring occurrences to an array of indexes.
+     *
+     * @param substring substring to look for
+     * @param in input file
+     * @return list of all the occurrences of a given substring
+     */
+    public static List<Integer> search(String substring, InputStream in)
+            throws NullPointerException, IOException {
+        if (substring == null || in == null) {
+            throw new NullPointerException();
+        }
+        int len = substring.length();
 
         if (len == 0) {
-            return 0;
+            return null;
         }
 
-        ZZ.set(0, len);
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(in, StandardCharsets.UTF_8));
+        int[] zline = zline(substring);
+        int pos = max(len, 512);
+        char[] curr = new char[pos];
+        int off = 0;
+        int f = reader.read(curr, off, pos);
+        int[] zz = zfunction(curr, substring.toCharArray(), zline, f);
 
-        String line = reader.readLine();
-        StringBuilder zLine = new StringBuilder();
-        while (!line.isEmpty()) {
-            zLine.append(string);
-            zLine.append(line);
-            ArrayList<Integer> Z = z_function(zLine.toString());
-            ZZ.addAll(Z);
-        }
-        int res = 0;
-        for (Integer i : ZZ) {
-            if(i.equals(len)) {
-                res++;
+        ArrayList<Integer> indexes = new ArrayList<>();
+        int cnt = 0;
+        for (int i = 0; i < zz.length; i++) {
+            if (zz[i] == len) {
+                indexes.add(cnt, i);
+                cnt++;
             }
         }
-        return res;
+
+        off += pos;
+        char[] prev = new char[len];
+        System.arraycopy(curr, curr.length - len, prev, 0, len);
+        f = reader.read(curr, 0, pos);
+        char[] str;
+        while (f > 0) {
+            str = (String.valueOf(prev) + String.valueOf(curr)).toCharArray();
+            zz = zfunction(str, substring.toCharArray(), zline, f);
+            System.arraycopy(curr, curr.length - len, prev, 0, len);
+
+            for (int i = 0; i < zz.length; i++) {
+                if (zz[i] == len && !indexes.contains(i + off - len)) {
+                    indexes.add(cnt, i + off - len);
+                    cnt++;
+                }
+            }
+
+            off += pos;
+            f = reader.read(curr, 0, pos);
+        }
+        return indexes;
     }
 
-    ArrayList<Integer> z_function(String line) {
-        int n = line.length();
-        ArrayList<Integer> Z = new ArrayList<>(n);
-        for (int i=1, l=0, r=0; i<n; ++i) {
+    /**
+     * Builds the z function of the given substring.
+     *
+     * @return array of integers representing z function
+     */
+    private static int[] zline(String substring) {
+        int[] z = new int[substring.length()];
+        int n = substring.length();
+        int l = 0;
+        int r = 0;
+        for (int i = 0; i < n; i++) {
             if (i <= r) {
-                Z.set(i, min(r - i + 1, Z.get(i - l)));
+                z[i] = min(r - i + 1, z[i - l]);
             }
-            while (i + Z.get(i) < n && line.charAt(Z.get(i)) == line.charAt(i+Z.get(i))) {
-                Z.set(i, Z.get(i)+1);
+            while (i + z[i] < n && substring.charAt(z[i]) == substring.charAt(i + z[i])) {
+                z[i]++;
             }
-            if (i+Z.get(i)-1 > r) {
+            if (i + z[i] - 1 > r) {
                 l = i;
-                r = i + Z.get(i) - 1;
+                r = i + z[i] - 1;
             }
         }
-        return  Z;
+        return z;
+    }
+
+    /**
+     * Builds the z function of a line using z function of the given substring.
+     *
+     * @param line line to build z function of
+     * @param substr substring to look for
+     * @param zline z function of a substring
+     * @param f number of characters read
+     * @return array of integers representing z function of a line
+     */
+    private static int[] zfunction(char[] line, char[] substr, int[] zline, int f) {
+        int[] zz = new int[line.length];
+        int l = -1;
+        int r = -1;
+        for (int i = 0; i < f; i++) {
+            if (i < r) {
+                zz[i] = zline[i - l];
+            } else {
+                zz[i] = 0;
+            }
+            while (zz[i] < zline.length
+                    && i + zz[i] < line.length && substr[zz[i]] == line[i + zz[i]]) {
+                zz[i]++;
+            }
+            if (i + zz[i] > r) {
+                l = i;
+                r = i + zz[i];
+            }
+        }
+        return zz;
     }
 }
---
+++ Task_1_3_1/src/test/java/io/github/jokerhasnopersonality/StringSearchTest.java
@@ -0,0 +1,60 @@
+package io.github.jokerhasnopersonality;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Tests to check StringSearch realization.
+ */
+public class StringSearchTest {
+    @Test
+    public void test0() throws IOException {
+        String path = "/Test1.txt";
+        Assertions.assertThrows(NullPointerException.class, () -> StringSearch.search(null,
+                StringSearch.class.getResourceAsStream(path)));
+        Assertions.assertThrows(NullPointerException.class,
+                () -> StringSearch.search("dreams", null));
+    }
+
+    @Test
+    public void test1() throws IOException {
+        String path = "/Test1.txt";
+        InputStream in = StringSearch.class.getResourceAsStream(path);
+        List<Integer> indexes = StringSearch.search("dreams", in);
+        List<Integer> test = List.of(6, 265);
+        Assertions.assertEquals(2, indexes.size());
+        Assertions.assertEquals(test, indexes);
+        in.close();
+
+        in = StringSearch.class.getResourceAsStream(path);
+        indexes = StringSearch.search("to", in);
+        test = List.of(39, 148, 177, 214, 245, 298);
+        assert indexes != null;
+        Assertions.assertEquals(6, indexes.size());
+        Assertions.assertEquals(test, indexes);
+        in.close();
+    }
+
+    @Test
+    public void test2() throws IOException {
+        String path = "/Test2.txt";
+        InputStream in = StringSearch.class.getResourceAsStream(path);
+        List<Integer> indexes = StringSearch.search("abc", in);
+        List<Integer> test = List.of(0, 3, 6, 9, 12, 40, 52);
+        Assertions.assertEquals(7, indexes.size());
+        Assertions.assertEquals(test, indexes);
+        in.close();
+
+        in = StringSearch.class.getResourceAsStream(path);
+        indexes = StringSearch.search("abca", in);
+        test = List.of(0, 3, 6, 9);
+        assert indexes != null;
+        Assertions.assertEquals(4, indexes.size());
+        Assertions.assertEquals(test, indexes);
+        in.close();
+    }
+}
