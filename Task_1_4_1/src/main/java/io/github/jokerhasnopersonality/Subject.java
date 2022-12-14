6bc133aa89ec53d8535255e08cd441fea8644379
Q Taynaya <3773050@mail.ru>
2022-12-14 17:25:23 +0700

    Small changes.

--- Task_1_4_1/src/main/java/io/github/jokerhasnopersonality/Subject.java
+++ Task_1_4_1/src/main/java/io/github/jokerhasnopersonality/Subject.java
@@ -1,9 +1,10 @@
 package io.github.jokerhasnopersonality;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Subject {
-    private ArrayList<Subject> grades;
+    private final List<Grade> grades;
     private String subject;
     private int cnt;
     private double average;
@@ -13,14 +14,20 @@ public class Subject {
         this.subject = subject;
         grades = new ArrayList<>();
         this.semester = semester;
+        cnt = 0;
+        average = 0;
     }
 
-    public void addGrade(Grade grade) {
-        return;
+    public void addGrade(Grade.EvaluationType type, int grade) {
+        Grade add = new Grade(type, grade);
+        grades.add(add);
+        if (type != Grade.EvaluationType.PASS) {
+            average = grades.stream().toArray().
+        }
     }
 
     public Grade getGrade(int index) {
-        return null;
+        return grades.get(index);
     }
 
     public double getAverage() {
