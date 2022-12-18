8063f3a51fdbe6f27eba619d6c1f6c21caebbcc7
Q Taynaya <3773050@mail.ru>
2022-12-18 23:06:49 +0700

    Fixing style...

--- Task_1_4_1/src/main/java/io/github/jokerhasnopersonality/Grade.java
+++ Task_1_4_1/src/main/java/io/github/jokerhasnopersonality/Grade.java
@@ -1,22 +1,31 @@
 package io.github.jokerhasnopersonality;
 
+/**
+ * Class representing a grade in a student gradebook.
+ */
 public class Grade {
-    /* For EXAM and CREDIT types evaluation system is:
-       5 - Excellent,
-       4 - Good,
-       3 - Satisfactory,
-       2 - Unsatisfactory.
-       For PASS type evaluation system is:
-       5 - Pass,
-       0 - Fail.
+    /**
+     * For EXAM and CREDIT types evaluation system is:
+     *        5 - Excellent,
+     *        4 - Good,
+     *        3 - Satisfactory,
+     *        2 - Unsatisfactory.
+     *        For PASS type evaluation system is:
+     *        5 - Pass,
+     *        0 - Fail.
      */
     public enum EvaluationType {
         EXAM,
         CREDIT,
         PASS
     }
+
     private EvaluationType type;
     private int grade;
+
+    /**
+     * Grade constructor. Initiates a grade with the specified evaluation type and value.
+     */
     public Grade(EvaluationType type, int grade) throws IllegalStateException {
         if ((grade < 0 || grade > 5) || (type == null)
                 || (type == EvaluationType.PASS && grade != 0 && grade != 5)
