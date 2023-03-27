176da7d292844d9ef8f4109df04a3363339d4e6e
Q Taynaya <3773050@mail.ru>
2023-03-28 02:37:55 +0700

    Fixing reviedog...

--- Task_2_2_1/src/test/java/io/github/jokerhasnopersonality/PizzeriaTest.java
+++ Task_2_2_1/src/test/java/io/github/jokerhasnopersonality/PizzeriaTest.java
@@ -1,9 +1,9 @@
 package io.github.jokerhasnopersonality;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import java.io.IOException;
 
 /**
  * Test for Pizzeria class.
