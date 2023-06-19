79c6fb66fa01093155a5ec0acfb4af19795ae643
Q Taynaya <3773050@mail.ru>
2023-06-19 23:48:31 +0700

    Trying to fix build...

---
+++ Task_2_4_1/src/test/java/io/github/jokerhasnopersonality/TaskInspectorTest.java
@@ -0,0 +1,12 @@
+package io.github.jokerhasnopersonality;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for TaskInspector.
+ */
+public class TaskInspectorTest {
+    @Test
+    public void simpleTest() {
+    }
+}
